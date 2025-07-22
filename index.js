dd1abbf87bbb436e0718af364ca1b266de85b215
gautami2318 <165652806+gautami2318@users.noreply.github.com>
2025-07-22 03:00:10 -0700

    index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require ('express')
-const app = express
-const port = 3001
+const express = require('express');
+const app = express();  
+const port = 3001;
 
-app.get('/hello',(req,res) => {
-    app.send("hello")
-})
+app.get('/hello', (req, res) => {
+    res.send("hello");   
+});
 
-app.get('/time',(req,res) => {
-    app.send("time")
-})
+app.get('/time', (req, res) => {
+    res.send("time");
+});
 
-app.get('/date',(req,res) => {
-    app.send("date")
-})
+app.get('/date', (req, res) => {
+    res.send("date");
+});
 
-app.listen(port,() =>{
-    console.log('Example app listening a http://localhost:${port}')
-})
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);  
+});
